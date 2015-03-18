9f22e3dccab2319dfada4845eabaeb6cfa79d9fc
D-Buettner <deric.buettner@gmail.com>
2015-03-18 14:04:09 +0100

    Collision detection works

--- game.js
+++ game.js
@@ -32,8 +32,10 @@ Game.prototype.toString = function() {
     row.forEach(function(col){
       if (col[0] === ".") {
         resultString += ".";
-      } else if (col[0] === "o" || "O") {
+      } else if (col[0] === "o" || col[0] === "O") {
         resultString += "o";
+      } else if (col[0] === "#") {
+        resultString += "#";
       }
     });
     resultString += '<br>';
@@ -42,27 +44,60 @@ Game.prototype.toString = function() {
 };
 
 Game.prototype.step = function(objRef) {
+
+  // Maybe refactor this
+
   // objRef needed to keep this binding the same.
+  var moveQueue = [];
   if (!objRef) {
     var objRef = this;
   }
+  if (objRef.gotShape === false) {
+    objRef.newShape();
+  }
+
   console.log("step function");
   // Go through rows backwards to avoid moving blocks more than once.
   for (var row = objRef.height - 1; row >= 0; row--) {
-    console.log('going through row: ', row);
+    console.log("row: ", row);
     objRef.board[row].forEach(function(element, column) {
-      if (element !== ".") {
-        console.log("col index is: ", column, "col is: ", element);
-        objRef.board[row + 1][column] = element;
-        objRef.board[row][column] = ".";
-        
-      }
+      // Check square below.
+      if (element === "o" || element === "O") {
+        var nextRow = row + 1;
+        // Run collision detect then move if needed
+        var didCollide = objRef.collisionDetect(nextRow, column);
+        if (!didCollide) {
+
+          console.log("setting point", nextRow, column);
+          objRef.setPoint(nextRow, column, element);
+          objRef.setPoint(row, column, ".");
+          console.log("shape moved to row: ", nextRow);
+        }
+      };
     });
   }
   objRef.toString();
   console.log("creating new timer..");
   var nextStep = setInterval(this.step, 1000, objRef);
 };
+
+Game.prototype.convertShape = function() {
+  var objRef = this;
+  console.log("calling convert shape");
+  this.board.forEach(function(row, rowIndex) {
+    console.log('hey', row, rowIndex);
+    row.forEach(function(element, elIndex) {
+      if (element === "o" || element === "O") {
+
+        console.log("block detected");
+        console.log("row is... ",row,"elIndex is..",elIndex,"element is..",element);
+        objRef.setPoint(rowIndex, elIndex, "#");
+        console.log(objRef.board);
+        objRef.gotShape = false;
+      }
+    });
+  });
+}
 
 Game.prototype.newShape = function() {
   var objRef = this;
@@ -82,16 +117,39 @@ Game.prototype.newShape = function() {
   console.log(this.board);
 };
 
+Game.prototype.collisionDetect = function(row, column) {
+  var objRef = this;
+  var collision = false;
+  console.log('function collision detect for row: ', row, " and column: ", column);
+  console.log(this.getPoint(row, column));
+  console.log(objRef.board)
+  if (objRef.getPoint(row, column) === undefined) {
+    objRef.convertShape();
+    return true;
+  }
+  objRef.board[row].forEach(function(col, colIndex) {
+    if (objRef.getPoint(row, colIndex) === "#") {
+      console.log("collision");
+      objRef.convertShape();
+      collision = true;
+    }
+  });
+  return collision;
+}
+
 // x and y measured from top-left
 
-Game.prototype.getPoint = function(x, y) {
-
-  var itemAtCoordinates = this.board[x][y];
+Game.prototype.getPoint = function(row, col) {
+  if (!this.board[row]) {
+    return undefined;
+  }
+  var itemAtCoordinates = this.board[row][col];
   return itemAtCoordinates;
 };
 
-Game.prototype.setPoint = function(x, y, item) {
-  this.board[x][y] = item;
+Game.prototype.setPoint = function(row, col, item) {
+  console.log("setting item at..", row, col);
+  this.board[row][col] = item;
 };
 
 function createBoard(height, width) {
