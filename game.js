229456f94b9e9a65be3e1d91fe2cdaa286befe5c
D-Buettner <deric.buettner@gmail.com>
2015-03-18 18:19:06 +0100

    Blocks move left or right.

--- game.js
+++ game.js
@@ -22,13 +22,13 @@ function Game(height, width, level) {
   this.board = createBoard(height, width);
   this.shapeList = shapes;
   this.gotShape = false;
-  console.log(this.height, this.width);
 }
 
 Game.prototype.toString = function() {
+  console.log("toString called");
   var resultString =  "";
+  // row and column measured from top-left.
   this.board.forEach(function(row) {
-    console.log()
     row.forEach(function(col){
       if (col[0] === ".") {
         resultString += ".";
@@ -44,14 +44,13 @@ Game.prototype.toString = function() {
 };
 
 Game.prototype.step = function(objRef) {
-
-  // Maybe refactor this
-
-  // objRef needed to keep this binding the same.
-  var moveQueue = [];
+  // set objRef to pass to interval function.
   if (!objRef) {
     var objRef = this;
   }
+  // Maybe refactor this
+
+  var moveQueue = [];
   if (objRef.gotShape === false) {
     objRef.newShape();
   }
@@ -59,85 +58,79 @@ Game.prototype.step = function(objRef) {
   console.log("step function");
   // Go through rows backwards to avoid moving blocks more than once.
   for (var row = objRef.height - 1; row >= 0; row--) {
-    console.log("row: ", row);
     objRef.board[row].forEach(function(element, column) {
       // Check square below.
       if (element === "o" || element === "O") {
+        console.log('element found');
         var nextRow = row + 1;
         // Run collision detect then move if needed
         var didCollide = objRef.collisionDetect(nextRow, column);
         if (!didCollide) {
-
-          console.log("setting point", nextRow, column);
           objRef.setPoint(nextRow, column, element);
           objRef.setPoint(row, column, ".");
-          console.log("shape moved to row: ", nextRow);
         }
       };
     });
   }
   objRef.toString();
-  console.log("creating new timer..");
+  console.log("step concluding")
   var nextStep = setInterval(this.step, 1000, objRef);
 };
 
 Game.prototype.convertShape = function() {
-  var objRef = this;
-  console.log("calling convert shape");
+  console.log("converting  shape");
   this.board.forEach(function(row, rowIndex) {
-    console.log('hey', row, rowIndex);
     row.forEach(function(element, elIndex) {
       if (element === "o" || element === "O") {
-
-        console.log("block detected");
-        console.log("row is... ",row,"elIndex is..",elIndex,"element is..",element);
-        objRef.setPoint(rowIndex, elIndex, "#");
-        console.log(objRef.board);
-        objRef.gotShape = false;
+        this.setPoint(rowIndex, elIndex, "#");
+        this.gotShape = false;
       }
-    });
-  });
+    }, this);
+  }, this);
 }
 
 Game.prototype.newShape = function() {
-  var objRef = this;
   // Select shape
+  console.log("New shape");
   var type = randomShape();
   for (var i = 0; i < this.shapeList.length; i++) {
     if (this.shapeList[i].name === type) {
       // Write shape to page from stored coordinate pairs (x=[0], y=[1])
       var chosenShape = this.shapeList[i];
       chosenShape.startingCoordinates.forEach(function(point) {
-        objRef.setPoint(point[0], point[1], "o");
-      });
-      objRef.setPoint(chosenShape.pivot[0], chosenShape.pivot[1], "O");
+        this.setPoint(point[0], point[1], "o");
+      }, this);
+      this.setPoint(chosenShape.pivot[0], chosenShape.pivot[1], "O");
     }
     this.gotShape = true;
   }
-  console.log(this.board);
 };
 
 Game.prototype.collisionDetect = function(row, column) {
-  var objRef = this;
+  console.log("collisionDetect called!!!");
   var collision = false;
-  console.log('function collision detect for row: ', row, " and column: ", column);
-  console.log(this.getPoint(row, column));
-  console.log(objRef.board)
-  if (objRef.getPoint(row, column) === undefined) {
-    objRef.convertShape();
+  console.log(this.board);
+  if (this.getPoint(row, column) === undefined) {
+    console.log("hit the bottom");
+    this.convertShape();
     return true;
   }
-  objRef.board[row].forEach(function(col, colIndex) {
-    if (objRef.getPoint(row, colIndex) === "#") {
-      console.log("collision");
-      objRef.convertShape();
-      collision = true;
+  this.board[row].forEach(function(col, colIndex) {
+
+    if (this.getPoint(row, colIndex) === "#") {
+      // Check cell above (redo this)
+      console.log
+      if (this.getPoint(row - 1, colIndex) !== ".") {
+        console.log(this.getPoint(row-1, colIndex), "above");
+        console.log(this.getPoint(row, colIndex), "below");
+        console.log("hit shape");
+        this.convertShape();
+        collision = true;
+      }
     }
-  });
+  }, this);
   return collision;
 }
-
-// x and y measured from top-left
 
 Game.prototype.getPoint = function(row, col) {
   if (!this.board[row]) {
@@ -151,6 +144,75 @@ Game.prototype.setPoint = function(row, col, item) {
   console.log("setting item at..", row, col);
   this.board[row][col] = item;
 };
+
+Game.prototype.getActiveBlockLocations =  function() {
+  // Returns a list of row col location pairs for active blocks
+  var blocks = [];
+  this.board.forEach(function(row, rowIndex) {
+    row.forEach(function(col, colIndex) {
+      if (col === "o" || col === "O") {
+        console.log(colIndex)
+        blocks.push({row:rowIndex, col:colIndex, blockType:col});
+      }
+    }, this);
+  });
+  return blocks;
+};
+
+Game.prototype.transformBlocks = function(blockList, f) {
+  console.log("transforming blocks");
+  // Wipe previous location
+  blockList.forEach(function(block){
+    this.setPoint(block.row, block.col, ".");
+  }, this);
+
+  blockList = blockList.map(f);
+
+  blockList.forEach(function(block){
+    console.log("TRANSFORRMMM", this);
+    this.setPoint(block.row, block.col, block.blockType);
+    console.log(this.board);
+  }, this);
+}
+
+
+Game.prototype.move = function(direction) {
+  console.log("moving", direction);
+
+  if (direction === "left") {
+    var transform = -1;
+  } else if (direction === "right") {
+    var transform = 1;
+  }
+
+  var blockList = this.getActiveBlockLocations();
+
+  // Check if legal move
+  var isLegal = true
+  blockList.forEach(function(block){
+    console.log("checking legality");
+    var adjacentChar = this.getPoint(block.row, block.col + transform);
+    if (adjacentChar === undefined || adjacentChar === "#") {
+      console.log("illegal");
+      isLegal = false;
+    }
+  }, this);
+
+  if (!isLegal) {
+    return;
+  }
+
+  this.transformBlocks(blockList, function(block) {
+      console.log("this", block);
+      block.col += transform;
+      console.log(block);
+      return block;
+  });
+  this.toString();
+};
+
+Game.prototype.rotateClockwise = function() {};
+Game.prototype.drop = function() {};
 
 function createBoard(height, width) {
 
@@ -182,8 +244,31 @@ function writeToWindow(string) {
     target.innerHTML = string;
 }
 
+function addKeyboardControl(GameObject) {
+  document.addEventListener("keydown", function(event){
+    switch (event.keyCode) {
+      case 37:
+        GameObject.move("left");
+        break;
+      case 39:
+        GameObject.move("right");
+        break;
+      case 38:
+        GameObject.rotateClockwise();
+        break;
+      case 40:
+        GameObject.drop();
+        break;
+
+    }
+
+
+  });
+}
+
 function run(height, width, level) {
   var currentGame = new Game(height, width, level);
+  addKeyboardControl(currentGame);
   currentGame.newShape();
   currentGame.step();
 }
